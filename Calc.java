e5d0109951413b8e308ef9eaa178772f3b41c7ca
chowdeswar36 <97867239+chowdeswar36@users.noreply.github.com>
2022-02-09 14:39:13 +0530

    added sub method

--- Calc.java
+++ Calc.java
@@ -2,7 +2,10 @@ public class Calc{
 public static int add(int a,intb){
 return a_b;
 }
-public static int mul(int a,intb){
+public static int sub(int a,intb){
+return a-b;
+}
+ public static int mul(int a,intb){
 return a*b;
 }
 public static void main(){
